ec383918a08aa742444ca9d8f0a444246b1a8f13
philarmino <github@pmuendlein.de>
2018-08-14 20:08:42 +0200

    Update kali_hardening.sh

--- kali_hardening.sh
+++ kali_hardening.sh
@@ -2,4 +2,16 @@
 #
 # by philarmino
 
+
+# Requirements
+
+apt-get install pv 
+
+# SSH 
+
+echo -e "\nSet PermitRootLogin to no in sshd_config ...\n" | pv -qL 23
+sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
+
+
+
 # 
